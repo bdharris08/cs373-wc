b3f0b60772d0ae1acfccf9ed429d0e5dc3191a7a
Rohan Shah <rohan8@gmail.com>
2012-07-02 14:57:00 -0500

    Updated TestWC1.py

--- TestWC1.py
+++ TestWC1.py
@@ -18,33 +18,70 @@ class webTest(unittest.TestCase):
         self.assertTrue('Splash Page' in response)    
     
     def test_tibet(self):
+        app = TestApp(self.application)
+        response = app.get('/tibet')
+        self.assertTrue('Crisis: Tibetan Occupation' in response) 
     
     def test_gec(self):
-    
+        app = TestApp(self.application)
+        response = app.get('/gec')
+        self.assertTrue('Crisis: Global Financial Crisis' in response)     
     def test_bathsalts(self):
-    
+        app = TestApp(self.application)
+        response = app.get('/bathsalts')
+        self.assertTrue('Crisis: Bath Salts' in response)     
     def test_nkorea(self):
-    
+        app = TestApp(self.application)
+        response = app.get('/nkorea')
+        self.assertTrue('Crisis: North Korea and Nuclear Weapons' in response)     
     def test_UN(self):
-    
+        app = TestApp(self.application)
+        response = app.get('/un')
+        self.assertTrue('Organization: United Nations' in response) 
     def test_fed(self):
+        app = TestApp(self.application)
+        response = app.get('/frs')
+        self.assertTrue('Organization: The Federal Reserve' in response)         
     
     def test_dea(self):
+        app = TestApp(self.application)
+        response = app.get('/dea')
+        self.assertTrue('Organization: Drug Enforcement Administration' in response)         
     
     def test_dod(self):
+        app = TestApp(self.application)
+        response = app.get('/dod')
+        self.assertTrue('Organization: United States Department of Defense' in response)
     
     def test_dalailama(self):
+        app = TestApp(self.application)
+        response = app.get('/dali')
+        self.assertTrue('Person: Dalai Lama (Tenzin Gyatso)' in response)  
     
     def test_obama(self):
+        app = TestApp(self.application)
+        response = app.get('/obama')
+        self.assertTrue('Person: President Barack Obama' in response)         
     
     def test_leonhart(self):
+        app = TestApp(self.application)
+        response = app.get('/mml')
+        self.assertTrue('Person: Michele M. Leonhart' in response)  
     
     def test_kimjongun(self):
-    
+        app = TestApp(self.application)
+        response = app.get('/kju')
+        self.assertTrue('Person: Kim Jong-un' in response)
     def test_import(self):
+        app = TestApp(self.application)
+        response = app.get('/import')
+        self.assertTrue('Upload File' in response)         
     
     def test_export(self):
-
+        app = TestApp(self.application)
+        response = app.get('/export')
+        self.assertTrue('<worldCrises>' in response)
+        
 class ModelTest(unittest.TestCase):
     def test_new_Crisis_entity1(self):
         entity = model.Crisis()
