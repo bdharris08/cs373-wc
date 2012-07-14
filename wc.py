a29aee53ebb1ee479687c14dff3e735bd1d97097
bdharris08 <harris.benjamin@utexas.edu>
2012-07-14 13:57:57 -0500

    fixed wc2.xml human impact misc too long and wc.py

--- wc.py
+++ wc.py
@@ -144,65 +144,6 @@ class PersonHandler(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'personTemp.html')
         self.response.out.write(template.render(path, dictionary))
         
-class tibet(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('crisis-Tibet.html')
-        self.response.out.write(template.render())
-        
-class gec(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('crisis-GlobalFinancialCrisis.html')
-        self.response.out.write(template.render())
-        
-class bathsalts(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('crisis-BathSalts.html')
-        self.response.out.write(template.render())
-        
-class nkorea(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('crisis-NorthKorea.html')
-        self.response.out.write(template.render())
-
-class un(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('org-UN.html')
-        self.response.out.write(template.render())
-
-class frs(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('org-FRS.html')
-        self.response.out.write(template.render())
-
-class dea(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('org-DEA.html')
-        self.response.out.write(template.render())
-
-class dod(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('org-DepartmentOfDefense.html')
-        self.response.out.write(template.render())
-
-class dali(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('person_dalailama.html')
-        self.response.out.write(template.render())
-
-class mml(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('person_leonhart.html')
-        self.response.out.write(template.render())
-
-class obama(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('person_BarackObama.html')
-        self.response.out.write(template.render())
-        
-class kju(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('person_KimJungUn.html')
-        self.response.out.write(template.render())
         
 class ImportHandler(webapp.RequestHandler):
   def get(self):
@@ -906,11 +847,7 @@ class OrganizationPerson (db.Model):
     person = db.ReferenceProperty(Organization, collection_name = 'orgPerson')
 
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/tibet', tibet), ('/gec', gec), 
-							('/bathsalts', bathsalts), ('/nkorea', nkorea), ('/un', un), 
-							('/frs', frs), ('/dea', dea), ('/dod', dod), 
-							('/dali', dali), ('/mml', mml), ('/obama', obama), 
-							('/kju', kju), ('/import', ImportHandler), ('/upload', UploadHandler),
+app = webapp2.WSGIApplication([('/', MainPage), ('/import', ImportHandler), ('/upload', UploadHandler),
                             ('/serve/([^/]+)?', ServeHandler), ('/export', ExportHandler), 
                             ('/crisis/([^/]+)?', CrisisHandler),
                             ('/org/([^/]+)?', OrgHandler), 
