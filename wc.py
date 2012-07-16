e5845b5e90f3f6194d2f825a22748a39104bb2af
Matthew Granado <mattg@utexas.edu>
2012-07-16 15:19:31 -0500

    Finished export

--- wc.py
+++ wc.py
@@ -766,17 +766,45 @@ class ExportHandler(webapp.RequestHandler):
             clist = [c]
             cinfo = c.info.fetch(None).pop()
             clist.append(cinfo)
-            clist.append(cinfo.time)
-            clist.append(cinfo.location)
-            clist.append(cinfo.humanImpact)
-            clist.append(cinfo.economicImpact)
-            clist.append(c.ref)
+            clist.append(cinfo.time.fetch(None).pop())
+            clist.append(cinfo.location.fetch(None).pop())
+            clist.append(cinfo.humanImpact.fetch(None).pop())
+            clist.append(cinfo.economicImpact.fetch(None).pop())
+            reflist = c.ref.fetch(None)
+            for pi in reflist :
+                if pi.ref_type == 'primaryImage' :
+                    clist.append(pi)
+            images = []
+            for i in reflist :
+                if i.ref_type == 'image' :
+                    images.append(i)
+            clist.append(images)
+            videos = []
+            for v in reflist :
+                if v.ref_type == 'video' :
+                    videos.append(v)
+            clist.append(videos)
+            socials = []
+            for s in reflist :
+                if s.ref_type == 'social' :
+                    socials.append(s)
+            clist.append(socials)
+            exts = []
+            for e in reflist :
+                if e.ref_type == 'ext' :
+                    exts.append(e)
+            clist.append(exts)
+            colist = []
             for co in COQuery :
                 if co.organization.name == c.name :
-                    clist.append(co)
+                    colist.append(co.crisis)
+            clist.append(colist)
+            cplist = []
             for cp in CPQuery : 
                 if cp.person.name == c.name :
-                    clist.append(cp)
+                    cplist.append(cp.crisis)
+            clist.append(cplist)
+
             crisisList.append(clist)
             
         for o in OrgQuery :
@@ -785,140 +813,88 @@ class ExportHandler(webapp.RequestHandler):
             olist.append(oinfo)
             ocontact = oinfo.contact.fetch(None).pop()
             olist.append(ocontact)
-            olist.append(ocontact.mail)
-            olist.append(oinfo.location)
-            olist.append(o.ref)
+            olist.append(ocontact.mail.fetch(None).pop())
+            olist.append(oinfo.location.fetch(None).pop())
+            reflist = o.ref.fetch(None)
+            for pi in reflist :
+                if pi.ref_type == 'primaryImage' :
+                    olist.append(pi)
+            images = []
+            for i in reflist :
+                if i.ref_type == 'image' :
+                    images.append(i)
+            olist.append(images)
+            videos = []
+            for v in reflist :
+                if v.ref_type == 'video' :
+                    videos.append(v)
+            olist.append(videos)
+            socials = []
+            for s in reflist :
+                if s.ref_type == 'social' :
+                    socials.append(s)
+            olist.append(socials)
+            exts = []
+            for e in reflist :
+                if e.ref_type == 'ext' :
+                    exts.append(e)
+            olist.append(exts)
+            colist = []
             for co in COQuery :
                 if co.crisis.name == o.name :
-                    olist.append(co)
+                    colist.append(co.organization)
+            olist.append(colist)
+            oplist = []
             for op in OPQuery :
                 if op.person.name == o.name :
-                    olist.append(op)
+                    oplist.append(op.organization)
+            olist.append(oplist)
             orgList.append(olist)
             
         for p in PeopleQuery :
             plist = [p]
             pinfo = p.info.fetch(None).pop()
             plist.append(pinfo)
-            plist.append(pinfo.birthdate)
-            plist.append(p.ref)
+            plist.append(pinfo.birthdate.fetch(None).pop())
+            reflist = p.ref.fetch(None)
+            for pi in reflist :
+                if pi.ref_type == 'primaryImage' :
+                    plist.append(pi)
+            images = []
+            for i in reflist :
+                if i.ref_type == 'image' :
+                    images.append(i)
+            plist.append(images)
+            videos = []
+            for v in reflist :
+                if v.ref_type == 'video' :
+                    videos.append(v)
+            plist.append(videos)
+            socials = []
+            for s in reflist :
+                if s.ref_type == 'social' :
+                    socials.append(s)
+            plist.append(socials)
+            exts = []
+            for e in reflist :
+                if e.ref_type == 'ext' :
+                    exts.append(e)
+            plist.append(exts)
+            cplist = []
             for cp in CPQuery :
                 if cp.crisis.name == p.name :
-                    plist.append(cp)    
+                    cplist.append(cp.person)
+            plist.append(cplist)   
+            oplist = []
             for op in OPQuery :
                 if op.organization.name == p.name :
-                    plist.append(op)
+                    oplist.append(op.person)
+            plist.append(oplist)
             personList.append(plist)
         
         path2 = os.path.join(os.path.dirname(__file__), 'export.xml')
         self.response.headers['Content-Type'] = 'text/xml'
         self.response.out.write(template.render(path2, {"crises": crisisList, "orgs" : orgList, "persons" : personList},'text/xml'))
-
-        '''
-        for c in CrisisQuery :
-            clist = [c]
-            for ci in CrisisInfoQuery :
-                if ci.crisis.name == c.name :
-                    clist.append(ci)
-                    for d in DateQuery :
-                        if d.crisisInfo.history == ci.history :
-                            clist.append(d)
-                    for l in LocationQuery :
-                        if l.crisisInfo == ci :
-                            clist += l
-                    for hi in HumanImpactQuery :
-                        if hi.crisisInfo == ci :
-                            clist += hi
-                    for ei in EconImpactQuery :
-                        if ei.crisisInfo == ci :
-                            clist += ei
-            for e in ExtLinkQuery :
-                if e.crisis == c :
-                    clist += e
-            for co in COQuery :
-                if co.organization == c :
-                    clist += co
-            for cp in CPQuery :
-                if cp.person == c :
-                    clist += cp
-            crisisList.append(clist)
-
-        for o in OrgQuery :
-            olist = [o]
-            for oi in OrgInfoQuery :
-                if oi.organization == o :
-                    olist += oi
-                    for c in ContactQuery :
-                        if c.orgInfo == oi :
-                            olist += c
-                            for a in FullAddrQuery :
-                                if a.contact == c :
-                                    olist += a
-                for l in LocationQuery :
-                    if l.orgInfo == oi :
-                        olist += l
-            for e in ExtLinkQuery :
-                if e.organization == o :
-                    olist += e
-            for co in COQuery :
-                if co.crisis == o :
-                    olist += co
-            for op in OPQuery :
-                if op.person == o :
-                    olist += op
-            orgList.append(olist)
-
-        for p in PeopleQuery :
-            plist = [p]
-            for pi in PeopleInfoQuery :
-                if pi.person == p :
-                    plist += pi
-                for d in DateQuery :
-                    if d.personInfo == pi :
-                        plist += d
-            for e in ExtLinkQuery :
-                if e.person == p :
-                    plist += e
-            for cp in CPQuery :
-                if cp.crisis == p :
-                    plist += cp
-            for op in OPQuery :
-                if op.organization == p :
-                    plist += op
-            personList.append(plist)
-        '''
-        '''
-        path2 = os.path.join(os.path.dirname(__file__), 'export.xml')
-        t = loader.get_template(path2)
-        c = Context({"crises": crisisList, "orgs" : orgList, "persons" : personList})
-        return HttpResponse(t.render(c), mimetype="text/xml")
-        '''  
-        '''    
-        path2 = os.path.join(os.path.dirname(__file__), 'export.xml')
-        c = app.test_request_context(path2)
-        return render_to_response(path2, {"crises": crisisList, "orgs" : orgList, "persons" : personList}, context_list, mimetype='text/xml')
-        '''
-
-
-        
-        #tree = ElementTree()
-        #treebuilder = tree.TreeBuilder()
-        #build tree using models
-        #write .xml file to blobstore using tree.write
-        #redirect to servehandler to output
-        '''
-        xml = '<?xml version="1.0" encoding="UTF-8"?>\n<worldCrises>\n\t<crises>'
-        for c in CrisisQuery:
-            xml = xml + c.to_xml()
-        '''
-        #self.response.headers['Content-Type']='text/xml; charset=utf-8'
-        #self.response.out.write(xml)
-        #self.redirect('/serve/%s' % blob_info.key())
-        '''
-        BlobQuery = blobstore.BlobInfo.gql("Order BY creation DESC")
-        blob = BlobQuery.get()
-        self.redirect('/serve/%s' % blob.key())
-        '''
         
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
